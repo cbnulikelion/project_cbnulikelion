73a7dce52c6888dc93d6fc63bb8f1c88e426ba58
cbnulikelion <cbnu@likelion.org>
2017-05-24 09:44:13 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,63 @@
+class HomeController < ApplicationController
+
+=begin
+  def 404
+  end
+
+  def about
+  end
+
+  def blog-home-1
+  end
+
+  def blog-home-2
+  end
+
+  def blog-post
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def full-width
+  end
+
+  def portfolio-1-col
+  end
+
+  def portfolio-2-col
+  end
+
+  def portfolio-3-col
+  end
+
+  def portfolio-4-col
+  end
+
+  def portfolio-item
+  end
+
+  def pricing
+  end
+
+  def services
+  end
+
+  def sidebar
+  end
+=end
+  def about
+  end
+  
+  def contact
+  end
+  
+  def index
+  end
+  
+  def services
+  end
+end
